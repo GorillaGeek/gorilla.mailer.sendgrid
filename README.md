0bd7b3d0fa4d4ccdfa38fb9aca1dbafa42ad5f61
danieloprado <danieloprado@outlook.com>
2016-06-21 16:22:39 -0300

    initial

---
+++ README.md
@@ -0,0 +1,43 @@
+Gorilla Geek Mailer
+===================
+
+Install from nuget: https://www.nuget.org/packages/Gorilla.Mailer/
+
+```bash
+Install-Package Gorilla.Mailer
+```
+
+How to use
+----------
+
+```cs
+// Production
+// var mailer = Mailer.Create(enEmailProvider.SendGrid, "your-api-key");
+
+// Development
+var mailer = Mailer.CreateForDevelopement("path-to-folder-output");
+var providerResultKey = await mailer.Send("Subject", "From", "To", "Body");
+```
+
+Template
+--------
+
+From String:
+```cs
+var template = MailerTemplate.CreateFromString("Your template here [TemplateVariable]");
+template.Params.Add("TemplateVariable", "with variables");
+
+var body = template.Render();
+```
+
+From File:
+```cs
+var template = MailerTemplate.CreateFromFileSystem("path-to-file");
+```
+
+From Namespace:
+```cs
+// Remember to change the file property 'Buld Action' to 'Embedded Resource'
+var path = "MyApp.Templates.Email.html";
+var template = MailerTemplate.CreateFromManifestResourceStream(path);
+```
---
+++ src/SendGridMailer.cs
@@ -0,0 +1,46 @@
+﻿using Gorilla.Mailer.Exceptions;
+using Gorilla.Mailer.Interfaces;
+using SendGrid;
+using System;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Gorilla.Mailer.Providers.SendGrid
+{
+    public class SendGridMailer : IMailer
+    {
+        private readonly string _apiKey;
+
+        public SendGridMailer(string key)
+        {
+            _apiKey = key;
+        }
+
+        public async Task<string> Send(IMessage message)
+        {
+            var msg = new SendGridMessage();
+
+            msg.Subject = message.Subject;
+            msg.From = new MailAddress(message.From);
+            msg.Html = message.Body;
+
+            msg.AddTo(message.To);
+
+            msg.EnableClickTracking();
+            msg.EnableOpenTracking();
+
+            try
+            {
+                var transportWeb = new Web(_apiKey);
+                await transportWeb.DeliverAsync(msg);
+            }
+            catch (Exception ex)
+            {
+                throw new MailerException(ex.Message, MailerException.enReason.RejectByProvider);
+            }
+
+            return null;
+        }
+
+    }
+}
